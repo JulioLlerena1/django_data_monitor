799ab8f48d235191039401937534a413ae6ad665
Christian Palma Carpio <chripalm@espol.edu.ec>
2026-08-17 19:50:17 +0000

    autenticación

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -134,3 +134,20 @@ STATICFILES_DIRS = [
 ]
 
 API_URL = 'https://chripalm.pythonanywhere.com/landing/api/index/'
+
+CSRF_TRUSTED_ORIGINS = [
+  "https://*.app.github.dev", # Solo si utiliza Codespaces
+  "https://localhost:8000",
+  "http://127.0.0.1:8000"
+]
+
+ALLOWED_HOSTS = [
+  "*",
+]
+
+# Fallo: acceso sin autenticación
+LOGIN_URL = '/login/'
+
+# Éxito: luego de autenticación exitosa
+LOGIN_REDIRECT_URL = '/'
+
